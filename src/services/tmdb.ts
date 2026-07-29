67a9e90d954326a6a678fb8e5a8652dcb03d2862
thy <213379314+okaythi@users.noreply.github.com>
2026-07-29 02:44:02 +0100

    Secure TMDB token by using a backend proxy

--- src/services/tmdb.ts
+++ src/services/tmdb.ts
@@ -1,9 +1,7 @@
-const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_API_TOKEN = import.meta.env.VITE_TMDB_API_TOKEN;
+const TMDB_BASE_URL = "/api/tmdb";
 
 const fetchConfig = {
   headers: {
-    Authorization: `Bearer ${TMDB_API_TOKEN}`,
     "Content-Type": "application/json",
   },
 };
