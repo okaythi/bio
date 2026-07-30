bdede5c73da226e5de1e433a4515a1f0905e83b4
thy <213379314+okaythi@users.noreply.github.com>
2026-07-30 23:26:27 +0100

    refactor(config): Extract centralized configuration

---
+++ src/config/constants.ts
@@ -0,0 +1,16 @@
+export const SYSTEM_BUCKETS = ["2026-07_public_beta_v1", "2026-07_beta_user", "public_beta_v1", "beta_user"];
+
+export const ALLOWED_EXPERIMENT_BUCKETS = [
+  "2026-07_public_beta_v1",
+  "2026-07_beta_user",
+  "2026-07_auto_play_next_video",
+  "2026-07_smart_recommendations_v2",
+  "2026-07_hero_video_v2",
+  "2026-07_4k_player_beta",
+  "2026-07_ai_subtitles_v1",
+  "public_beta_v1",
+  "beta_user",
+  "auto_play_next_video"
+];
+
+export const KNOWN_FLAGS = ["is_staff", "edit_flags", "vip", "moderator"];
